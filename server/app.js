2ef91c711110e4f7c8e30064c2b8c8f434ccf85b
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2020-03-12 13:24:24 -0700

    Add MongoDB Schema to server app.js

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
@@ -8,4 +9,43 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 app.listen(port, () => {
   console.log(`listenning on port ${port}`);
 });
+
+mongoose.connect('mongodb://localhost/bar-comp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to DB!');
+});
+
+const streamerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true
+  },
+  avatarPicUrl: String,
+  backgroundPicUrl: String,
+  subscriptionEmoteSetUrl: String,
+  customEmoteSetURL: String,
+  customEmoteCount: Number,
+});
+
+const Streamer = mongoose.model('Streamer', streamerSchema);
+
+let test1 = new Streamer({
+  name: 'JohnDow',
+  avatarPicUrl: 'fakeURL1.com',
+  backgroundPicUrl: 'fakeURL2.com',
+  suscriptionEmotesSetUrl: 'fakeURL3.com',
+  customEmotesSetURL: 'fakeURL4.com',
+  customEmotesCount: 44
+});
+
+test1.save((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Successfully inserted into DB!');
+})
 
